2095224d78393f2d5962159d0db01e085039642c
PVM-05 <pvmthere@gmail.com>
2025-05-23 14:34:25 +0700

    Thêm độ dài rộng khung game tính đơn vị từng ô pixel của ô

--- src/snake/GamePanel.java
+++ src/snake/GamePanel.java
@@ -4,19 +4,61 @@
  */
 package snake;
 
-import java.awt.event.ActionListener;
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
-/**
- *
- * @author PVM
- */
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 public class GamePanel extends JPanel implements ActionListener {
-    GamePanel(){
+    
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH / UNIT_SIZE) * (SCREEN_HEIGHT / UNIT_SIZE);
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    GamePanel() {
+        
+    }
+    public void startGame() {
+        
+    }
+    public void paintComponent(Graphics g) {
+        
+    }
+    public void draw(Graphics g) {
+        
+    }
+    public void move() {
+        
+    }
+    public void checkApple() {
+        
+    }
+    public void checkCollision() {
+        
+    }
+    public void gameOver(Graphics g) {
         
     }
     @Override
     public void actionPerformed(ActionEvent e){
         
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            
+        }
     }
 }
