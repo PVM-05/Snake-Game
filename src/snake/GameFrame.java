951495ba011d9fae3552520f5c5d615ec0417f5f
PVM-05 <pvmthere@gmail.com>
2025-05-27 21:55:52 +0700

    Hoan thien game them am thanh background gameover va an tao ve lai ran

--- src/snake/GameFrame.java
+++ src/snake/GameFrame.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package snake;
 
+import javax.swing.*;
 
-import javax.swing.JFrame;
-
-public class GameFrame extends JFrame{
-    GameFrame(){
-        GamePanel Panel  = new GamePanel();
-        this.add(Panel);
-        this.setTitle("Ran san moi"); // dat tieu do cho cua so game
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // dam bao dong app khi tat cua so
-        this.setResizable(false); //khong cho thay doi kich thuoc cua so
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.setTitle("Rắn Săn Mồi By PVM :D");
+        this.add(new GamePanel());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
         this.pack();
-        this.setLocationRelativeTo(null); //Căn giữa cửa sổ trên màn hình.
-        this.setVisible(true); //Hiển thị cửa sổ.
+        this.setLocationRelativeTo(null);
     }
-    
 }
